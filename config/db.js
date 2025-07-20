ed361559050b704461b3e99488d0d3894c75fb27
Fardin Khan <fardinofficial@gmail.com>
2025-07-21 02:05:58 +0600

    done

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.0e0mm9t.mongodb.net/appointmentDB`);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error('MongoDB connection error:', error.message);
